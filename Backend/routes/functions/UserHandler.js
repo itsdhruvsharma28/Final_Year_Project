54f08d4b1cb399a3d620a6595d52f4aad15adbe0
itsdhruvsharma28 <dhruvsharma6377122@gmail.com>
2025-08-27 14:36:18 +0530

    backend update

--- Backend/routes/functions/UserHandler.js
+++ Backend/routes/functions/UserHandler.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const { userModel } = require("../../Models/userModel");
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 const jwtPassword = "8t42nfq9"
 
